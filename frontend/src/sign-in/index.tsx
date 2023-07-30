2f98e76b44974c16d5c6fef20a99442c38b25d36
Bishwendu Kundu <bishwenduk029@gmail.com>
2023-07-31 00:53:53 +0530

    remove unwanted files

--- frontend/src/sign-in/index.tsx
+++ frontend/src/sign-in/index.tsx
@@ -6,7 +6,7 @@ export default function SignInPage() {
   return (
     <Container>
       <Box m={100} height={"100%"}>
-        <SignIn afterSignInUrl="/chats" />
+        <SignIn afterSignInUrl="/bots/user_2ScqfpvP9Ef9xtQD2ey9gyHPy4K" />
       </Box>
     </Container>
   );
